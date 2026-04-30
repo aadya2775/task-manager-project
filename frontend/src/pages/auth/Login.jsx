c484ec48c62e3f90deb8a3fa29bdf67cac64f515
aadya2775 <aadya2775@gmail.com>
2026-05-01 01:57:55 +0530

    auth

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -90,7 +90,7 @@ const Login = () => {
               </div>
 
               <h1 className="text-2xl font-bold text-gray-800 mt-4 uppercase">
-                Project Flow
+                Task Manager
               </h1>
 
               <p className="text-gray-600 mt-1">
--- frontend/src/pages/auth/SignUp.jsx
+++ frontend/src/pages/auth/SignUp.jsx
@@ -87,7 +87,7 @@ const SignUp = () => {
               </div>
 
               <h1 className="text-2xl font-bold text-gray-800 mt-4 uppercase">
-                Join Project Flow
+                  Task Manager
               </h1>
 
               <p className="text-gray-600 mt-1">
